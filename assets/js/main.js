5cccbd7c6ebbdca076928efcba934475ee323473
jensen <jenstation925@gmail.com>
2024-04-11 19:36:25 -0500

    mi portafolio actualizado

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+import parallax from './components/parallax.js';
+import activeMenu from './components/selected_menu.js';
+import updateCopyright from './helpers/date_updater.js';
+import resetToHome from './helpers/reload_page.js';
+import sendEmail from './helpers/send_form.js';
+import changeLanguage from './helpers/lang.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    parallax();
+
+    activeMenu();
+
+    resetToHome();
+
+    updateCopyright();
+
+    sendEmail();
+
+    changeLanguage();
+
+});
